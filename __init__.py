4b86739e72599a1abaff7732d8250e6b574f5e10
Druzinin <114361040+Druzinin@users.noreply.github.com>
2023-05-22 00:20:45 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,75 +1,47 @@
+from random import randint
+
+
 class Matrix:
 
     def __init__(self, rows, cols):
         self.rows = rows
         self.cols = cols
-        self.grid = [[0] * cols] * rows
+        self.grid = [[]]
 
     def __str__(self):
-        output = ''
-        for i in range(self.rows):
-            row = ''
-            for j in range(self.cols):
-                row += str(self.grid[i][j]) + '\t'
-            output += row + '\n'
-        return output
+        return '\n'.join(['\t'.join([str(j) for j in i]) for i in self.grid]) + '\n'
+
+    @staticmethod
+    def out(lst):
+        return '\n'.join(['\t'.join([str(j) for j in i]) for i in lst]) + '\n'
 
     def __add__(self, other):
-        if self.rows != other.rows or self.cols != other.cols:
-            return None
-        else:
-            result = Matrix(self.rows, self.cols)
-            for i in range(self.rows):
-                for j in range(self.cols):
-                    result.grid[i][j] = self.grid[i][j] + other.grid[i][j]
-            return result
+        return self.out([[self.grid[i][j] + other.grid[i][j] for j in range(self.cols)] for i in range(self.rows)])
 
     def __sub__(self, other):
-        if self.rows != other.rows or self.cols != other.cols:
-            return None
-        else:
-            result = Matrix(self.rows, self.cols)
-            for i in range(self.rows):
-                for j in range(self.cols):
-                    result.grid[i][j] = self.grid[i][j] - other.grid[i][j]
-            return result
+        return self.out([[self.grid[i][j] - other.grid[i][j] for j in range(self.cols)] for i in range(self.rows)])
 
     def __mul__(self, other):
-        if self.cols != other.rows:
-            return None
-        else:
-            result = Matrix(self.rows, other.cols)
-            for i in range(result.rows):
-                for j in range(result.cols):
-                    total = 0
-                    for k in range(self.cols):
-                        total += self.grid[i][k] * other.grid[k][j]
-                    result.grid[i][j] = total
-            return result
+        return self.out([[sum([self.grid[i][k] * other.grid[k][j] for k in range(self.cols)]) for j in range(self.cols)]
+                         for i in range(self.rows)])
 
 
 # создание матриц
-a = Matrix(2, 2)
-a.grid = [[1, 2], [3, 4]]
+a = Matrix(3, 3)
+a.grid = [[randint(1, 10) for _ in range(a.cols)] for _ in range(a.rows)]
 print('Матрица a')
 print(a)
 
-b = Matrix(2, 2)
-b.grid = [[5, 6], [7, 8]]
+b = Matrix(3, 3)
+b.grid = [[randint(1, 10) for _ in range(b.cols)] for _ in range(b.rows)]
 print('Матрица b')
 print(b)
 
-# сложение матриц
-c = a + b
 print('сложение матриц')
-print(c)
+print(a + b)
 
-# вычитание матриц
-d = a - b
 print('вычитание матриц')
-print(d)
+print(a - b)
 
-# умножение матриц
-e = a * b
 print('умножение матриц')
-print(e)
+print(a * b)
